c83238d08d2b52e24e005eaef0de818a9e91847d
Jakaria-Jihad <jakariahossen669@gmail.com>
2023-08-04 00:55:06 +0600

    Added the Account creation functionality with Firebase Authentication.

--- app/src/main/java/com/example/ephemranote/CreateAccountActivity.java
+++ app/src/main/java/com/example/ephemranote/CreateAccountActivity.java
@@ -1,14 +1,105 @@
 package com.example.ephemranote;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PatternMatcher;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CreateAccountActivity extends AppCompatActivity {
+    EditText userEmail, userPassword, confirmPassword;
+    Button createAccountbtn;
+    ProgressBar creationProgressbar;
+    TextView loginTextviewwbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        userEmail = findViewById(R.id.user_email_edited_text);
+        userPassword = findViewById(R.id.user_password_edited_text);
+        confirmPassword = findViewById(R.id.user_password_confirmed_edited_text);
+        createAccountbtn = findViewById(R.id.create_account_btn);
+        creationProgressbar = findViewById(R.id.onCreate_progressbar);
+        loginTextviewwbtn = findViewById(R.id.login_textView_btn_sgnpg);
+
+        createAccountbtn.setOnClickListener(v -> createAccount());
+        loginTextviewwbtn.setOnClickListener(v -> finish());
+    }
+    void createAccount(){
+        String email = userEmail.getText().toString();
+        String password = userPassword.getText().toString();
+        String confirm_password = confirmPassword.getText().toString();
+
+        boolean is_validated = validateData(email, password, confirm_password);
+        if (!is_validated){
+            return;
+        }else {
+            createAccountFirebase(email, password);
+        }
+
+
+    }
+
+    boolean validateData(String email, String password, String confirmpassword){
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            userEmail.setError("Email is invalid");
+            return false;
+        }
+        if (password.length() < 6){
+            userPassword.setError("Password is too short");
+            return false;
+        }
+        if (!password.equals(confirmpassword)){
+            confirmPassword.setError("Password doesn't match");
+            return false;
+        }
+        return true;
+    }
+
+    void createAccountFirebase(String email, String password){
+        changingProgress(true);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(CreateAccountActivity.this,
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        changingProgress(false);
+                        if (task.isSuccessful()){
+                            Toast.makeText(CreateAccountActivity.this, "Account created successfully, check email for verification", Toast.LENGTH_SHORT).show();
+                            firebaseAuth.getCurrentUser().sendEmailVerification();
+                            firebaseAuth.signOut();
+                            finish();
+                        }else {
+                            Toast.makeText(CreateAccountActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+    }
+
+    void changingProgress(boolean inProgress){
+        if (inProgress){
+            creationProgressbar.setVisibility(View.VISIBLE);
+            createAccountbtn.setVisibility(View.GONE);
+        }else {
+            creationProgressbar.setVisibility(View.GONE);
+            createAccountbtn.setVisibility(View.VISIBLE);
+        }
     }
 }
