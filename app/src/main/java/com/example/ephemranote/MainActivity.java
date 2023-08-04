da6f7347e6a5de6aa47d5dd736e2e2d15128ba86
Jakaria-Jihad <jakariahossen669@gmail.com>
2023-08-05 04:07:01 +0600

    Finally the logout button with function added.

--- app/src/main/java/com/example/ephemranote/MainActivity.java
+++ app/src/main/java/com/example/ephemranote/MainActivity.java
@@ -5,10 +5,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageButton;
+import android.widget.PopupMenu;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.Query;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,7 +39,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void showMenu(){
-
+        PopupMenu popupMenu = new PopupMenu(MainActivity.this, menuBtn);
+        popupMenu.getMenu().add("Logout");
+        popupMenu.show();
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getTitle() == "Logout"){
+                    FirebaseAuth.getInstance().signOut();
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                    finish();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
     void setupRecyclerView(){
         Query query = Utility.getCollecReferenceNotes().orderBy("timestamp", Query.Direction.DESCENDING);
